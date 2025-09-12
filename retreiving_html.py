1a2772780656d241ef1aadd0e09cc7f4f84c5148
00-Aryan <22f2000697@ds.study.iitm.ac.in>
2025-09-12 22:25:40 +0530

    first commit

---
+++ retreiving_html.py
@@ -0,0 +1,26 @@
+import requests
+from bs4 import BeautifulSoup
+
+url_get = 'https://hcraj.nic.in/cishcraj-jdp/JudgementFilters/'
+
+url_post_captcha = 'https://hcraj.nic.in/cishcraj-jdp/causelists/setgetcaptcha?'
+
+# url_post = 'https://hcraj.nic.in/cishcraj-jdp/judgement-filters/getrecord'
+
+# r = requests.get(url_get)
+r = requests.post(url_post_captcha)
+
+
+
+# payload = {
+#     'fromdate': '',
+#     'todate': '' ,
+#     'rpJudgment' : '' ,
+#     'captcha' : '' 
+# }
+
+with open('file1.html' , 'w' ,encoding="utf-8") as f:
+    f.write(r.text)
+
+# soup = BeautifulSoup(r.text)
+# soup.
